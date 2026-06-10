8d09c29ecf13eafa2ba85e628a31e7829255d914
ista1024 <ista1024@gmail.com>
2026-06-10 14:28:45 +1000

    init spring boot project
    Project: Gradle - Groovy
    Language: Java
    Spring Boot: 4.0.6
    Project Metadata: com.ista1024.card-processing-simulator
    Packaging: Jar
    Configuration: Properties
    Java: 21

---
+++ src/main/java/com/ista1024/card_processing_simulator/CardProcessingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.ista1024.card_processing_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardProcessingSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardProcessingSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ista1024/card_processing_simulator/CardProcessingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ista1024.card_processing_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardProcessingSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
